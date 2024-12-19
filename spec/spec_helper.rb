2c20928d86bb985f70744125e5622f9e39c1d5b3
Andrew Sullivan Cant <acant@sugarcrm.com>
2024-12-19 15:51:47 -0500

    Add simplecov JSON and lcov reporting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,21 @@ require 'bundler/setup'
 
 # Setup code coverage
 require 'simplecov'
-SimpleCov.start
+require 'simplecov-lcov'
+require 'simplecov_json_formatter'
+SimpleCov.start do
+  # NOTE: Include the lcov formatter for CodeClimate reporting.
+  # Even though the simplecov JSON format will be output and used by default,
+  # the test reporter does not handle it reliable.
+  # Using the lcov formatter is more reliable.
+  SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new(
+    [
+      SimpleCov::Formatter::HTMLFormatter,
+      SimpleCov::Formatter::JSONFormatter,
+      SimpleCov::Formatter::LcovFormatter
+    ]
+  )
+end
 
 require 'rspec/side_effects'
 
